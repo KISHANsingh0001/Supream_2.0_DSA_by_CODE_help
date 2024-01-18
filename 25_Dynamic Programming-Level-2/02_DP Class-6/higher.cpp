7c9c534c9ce912c0ce3a18e079c9c49a67cfb00f
kishan singh thakur <kishasinghthakur27@gmail.com>
2024-01-18 15:58:49 +0530

    Dp class -06

---
+++ 25_Dynamic Programming-Level-2/02_DP Class-6/higher.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    int solveUsingRecursion(int start, int end ) {
+        //base case
+        if(start >= end) {
+            return 0;
+        }
+
+        int ans = INT_MAX;
+        for(int i=start; i<=end; i++) {
+            ans = min(ans, i + max(solveUsingRecursion(start, i-1), solveUsingRecursion(i+1, end)));
+        }
+        return ans; 
+    }
+    int solveUsingMem(int start, int end,vector<vector<int> >& dp ) {
+        //base case
+        if(start >= end) {
+            return 0;
+        }
+
+        if(dp[start][end] != -1) {
+            return dp[start][end];
+        }
+
+        int ans = INT_MAX;
+        for(int i=start; i<=end; i++) {
+            ans = min(ans, i + max(solveUsingMem(start, i-1,dp), solveUsingMem(i+1, end,dp)));
+        }
+        dp[start][end] = ans;
+        return dp[start][end]; 
+    }
+
+    int solveUsingTabulation(int n) {
+        vector<vector<int> > dp(n+2, vector<int>(n+1, 0));
+        for(int start_index = n-1; start_index>=1; start_index--) {
+            for(int end_index = 1; end_index<=n; end_index++) {
+
+                if(start_index >= end_index) {
+                    //invalid range
+                    continue;
+                }
+                int ans = INT_MAX;
+                for(int i=start_index; i<=end_index; i++) {
+                    ans = min(ans, i + max(dp[start_index][i-1], dp[i+1][end_index] ) );
+                }
+                dp[start_index][end_index] = ans;
+                
+            }
+        }
+        return dp[1][n];
+    }
+
+    int getMoneyAmount(int n) {
+        int start = 1;
+        int end = n;
+        vector<vector<int> > dp(n+1, vector<int>(n+1, -1));
+        int ans = solveUsingTabulation(n);
+        return ans;
+    }
+};
---
+++ 25_Dynamic Programming-Level-2/02_DP Class-6/minCost.cpp
@@ -0,0 +1,83 @@
+class Solution {
+public:
+    int solveUsingRecursion(vector<int>& arr, map< pair<int,int>, int >& maxi, int s, int e) {
+        //base case
+        if(s > e) {
+            return 0;
+        }
+        if(s == e) {
+            return 0;
+        } 
+
+        int ans = INT_MAX;
+        for(int i=s; i<e; i++) {
+            //i is used here for partitioning
+            ans = min(ans, (maxi[{s, i}] * maxi[{i+1,e}]) + 
+                            solveUsingRecursion(arr,maxi, s, i) + 
+                            solveUsingRecursion(arr,maxi, i+1, e) );
+        }
+        return ans;
+    }
+
+    int solveUsingMem(vector<int>& arr, map< pair<int,int>, int >& maxi, int s, int e,vector<vector<int> >& dp) {
+        //base case
+        if(s > e) {
+            return 0;
+        }
+        if(s == e) {
+            return 0;
+        } 
+        if(dp[s][e] != -1) {
+            return dp[s][e];
+        }
+
+        int ans = INT_MAX;
+        for(int i=s; i<e; i++) {
+            //i is used here for partitioning
+            ans = min(ans, (maxi[{s, i}] * maxi[{i+1,e}]) + 
+                            solveUsingMem(arr,maxi, s, i,dp) + 
+                            solveUsingMem(arr,maxi, i+1, e, dp) );
+        }
+        dp[s][e] = ans;
+        return dp[s][e];
+    }
+
+    int solveUsingTabulation(vector<int>& arr,map< pair<int,int>, int >& maxi) {
+        int n = arr.size();
+        vector<vector<int> > dp(n+2, vector<int>(n+1, 0));
+
+        for(int s_index=n; s_index>=0; s_index--) {
+            for(int e_index=0; e_index<=n-1; e_index++) {
+                if(s_index >= e_index) {
+                    continue;
+                }
+                int ans = INT_MAX;
+                for(int i=s_index; i<e_index; i++) {
+                    //i is used here for partitioning
+                    ans = min(ans, (maxi[{s_index, i}] * maxi[{i+1,e_index}]) + dp[s_index][i] +  dp[i+1][e_index] );
+                }
+                dp[s_index][e_index] = ans;
+            }
+        }
+        
+        return dp[0][n-1];
+    }
+
+    int mctFromLeafValues(vector<int>& arr) {
+        //pre computation
+        map< pair<int,int>, int > maxi;
+        for(int i=0; i<arr.size(); i++) {
+            maxi[{i,i}] = arr[i];
+            for(int j=i+1; j<arr.size(); j++) {
+                maxi[{i,j}] = max(arr[j], maxi[{i,j-1}]);
+            }
+        }
+        int n = arr.size();
+        int start = 0;
+        int end = n-1;
+        vector<vector<int> > dp(n+1, vector<int>(n+1, -1));
+        int ans = solveUsingTabulation(arr,maxi);
+        return ans;
+
+    }
+};

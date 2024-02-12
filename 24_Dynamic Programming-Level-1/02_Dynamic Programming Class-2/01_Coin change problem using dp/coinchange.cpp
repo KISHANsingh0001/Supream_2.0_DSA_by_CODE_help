1241c8f2a9cfad6e03974ba6ab26367efaa9119c
kishan singh thakur <kishasinghthakur27@gmail.com>
2024-02-12 14:49:37 +0530

    Dp Class-02

--- 24_Dynamic Programming-Level-1/02_Dynamic Programming Class-2/01_Coin change problem using dp/coinchange.cpp
+++ 24_Dynamic Programming-Level-1/02_Dynamic Programming Class-2/01_Coin change problem using dp/coinchange.cpp
@@ -1,89 +1,116 @@
-class Solution {
-public:
-    int solveUsingRecursion(vector<int>& coins, int amount) {
-        //base case
-        if(amount == 0) {
-            return 0;
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <limits.h>
+using namespace std;
+// #define INT_MAX 2147483647
+// int solveUsingRec(vector<int> &coins, int amount)
+// {
+//     // Base case
+//     if (amount < 0)
+//     {
+//         return INT_MAX;
+//     }
+//     if (amount == 0)
+//     {
+//         return 0;
+//     }
+//     int mini = INT_MAX;
+//     for (int i = 0; i < coins.size(); i++)
+//     {
+//         int coin = coins[i];
+//         if (coin <= amount)
+//         {
+//             int recursionAns = solveUsingRec(coins, amount - coin);
+//             if (recursionAns != INT_MAX)
+//             {
+//                 int ans = 1 + recursionAns;
+//                 mini = min(mini, ans);
+//             }
+//         }
+//     }
+//     return mini;
+// }
+// int solveUsingMem(vector<int> &coins, int amount, vector<int> &dp)
+// {
+//     if (amount == 0)
+//     {
+//         return 0;
+//     }
+//     if (amount < 0)
+//     {
+//         return INT_MAX;
+//     }
+//     if (dp[amount] != -1)
+//     {
+//         return dp[amount];
+//     }
+//     int mini = INT_MAX;
+//     for (int i = 0; i < coins.size(); i++)
+//     {
+//         // curr coin
+//         int coin = coins[i];
+//         // yadi hamar coin vo amount se chota hai ya equal hai to hi use karge
+//         if (coin <= amount)
+//         {
+//             int recursionAns = solveUsingMem(coins, amount - coin, dp);
+//             if (recursionAns != INT_MAX)
+//             {
+//                 int ans = 1 + recursionAns;
+//                 mini = min(mini, ans);
+//             }
+//         }
+//     }
+//     dp[amount] = mini;
+//     return dp[amount];
+// }
+int solveUsingTabulation(vector<int> &coins, int amount, vector<int> &dp)
+{
+    // step 1 : create dp array
+    // vector<int> dp(amount + 1, INT_MAX);
+    // step 2: analayese the base case
+    dp[0] = 0;
+    for (int value = 1; value <= amount; value++)
+    {
+        int mini = INT_MAX;
+        for (int i = 0; i < coins.size(); i++)
+        {
+            // curr coin
+            int coin = coins[i];
+            // yadi hamar coin vo amount se chota hai ya equal hai to hi use karge
+            if (coin <= value)
+            {
+                int recursionAns = dp[value - coin];
+                if (recursionAns != INT_MAX)
+                {
+                    int ans = 1 + recursionAns;
+                    mini = min(mini, ans);
+                }
+            }
         }
+        dp[value] = mini;
+    }
+    return dp[amount];
+}
+int main()
+{
+    vector<int> coins = {6, 8, 5};
+    int amount = 11;
+    vector<int> dp(amount + 1, -1);
 
-        //recursive relation
-        int mini = INT_MAX;
-        for(int i=0; i<coins.size(); i++) {
-            //find ans using ith coin
-            //call recursion only for valid amounts i.e. >=0  wale amounts
-            if(amount - coins[i] >= 0) {
-                int recursionKaAns = solveUsingRecursion(coins, amount - coins[i]);
-                //if a valid answeer
-                if(recursionKaAns != INT_MAX) {
-                    //considering uage of ith coin
-                    int ans = 1 + recursionKaAns;
-                    mini  = min(mini, ans);
-                }
-            }
-        }
-        return mini;
+    int ans = solveUsingTabulation(coins, amount, dp);
+    // for (auto it : dp)
+    // {
+    //     cout << it << " ";
+    // }
+    // cout << endl;
+    if (ans == INT_MAX)
+    {
+        cout << "-1" << endl;
     }
-    int solveUsingMem(vector<int>& coins, int amount, vector<int>& dp) {
-        //base case
-        if(amount == 0) {
-            return 0;
-        }
-        //already ans exist
-        if(dp[amount] != -1) {
-            return dp[amount];
-        }
-        //recursive relation
-        int mini = INT_MAX;
-        for(int i=0; i<coins.size(); i++) {
-            //find ans using ith coin
-            //call recursion only for valid amounts i.e. >=0  wale amounts
-            if(amount - coins[i] >= 0) {
-                int recursionKaAns = solveUsingMem(coins, amount - coins[i], dp);
-                //if a valid answeer
-                if(recursionKaAns != INT_MAX) {
-                    //considering uage of ith coin
-                    int ans = 1 + recursionKaAns;
-                    mini  = min(mini, ans);
-                }
-            }
-        }
-        dp[amount] = mini;
-        return dp[amount];
+    else
+    {
+        cout << "Coins required is: " << ans << endl;
     }
-    int solveUsingTabulation(vector<int>& coins, int amount) {
-        //step1: done
-        int n = amount;
-        vector<int> dp(n+1, INT_MAX);
-        //step2: base case analyse
-        dp[0] = 0;
-        //for loop
-        for( int value = 1; value<=amount; value++) {
-            int mini = INT_MAX;
-            for(int i=0; i<coins.size(); i++) {
-            
-                if(value - coins[i] >= 0) {
-                    int recursionKaAns = dp[value - coins[i] ];
-                    
-                    if(recursionKaAns != INT_MAX) {
-                        
-                        int ans = 1 + recursionKaAns;
-                        mini  = min(mini, ans);
-                    }
-                }
-            }
-            dp[value] = mini;
-        }
-        return dp[amount];
-    }
-    int coinChange(vector<int>& coins, int amount) {
-        // int ans =  solveUsingRecursion(coins, amount);
-        //int n = amount;
-        //vector<int> dp(n+1, -1);
-        int ans = solveUsingTabulation(coins, amount);
-        if(ans == INT_MAX)
-            return -1;
-        else {
-            return ans;
-        }
-    }
-};
+    return 0;
+}
